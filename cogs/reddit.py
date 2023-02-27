403e1ed2c8db9505112357bf9500e383bace1c37
Kay <davidanton037@gmail.com>
2023-02-27 12:39:01 -0500

    Fixed reddit bug

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -5,11 +5,6 @@ import random
 from discord.ext import commands
 from utils.constants import REDDIT_CLIENT, REDDIT_SECRET
 
-# Initialize the reddit instance
-reddit = asyncpraw.Reddit(client_id=REDDIT_CLIENT,
-                     client_secret=REDDIT_SECRET,
-                     user_agent='bot')
-
 class reddit(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -17,6 +12,12 @@ class reddit(commands.Cog):
     # Random meme command 
     @commands.command(aliases=["memes"])
     async def meme(self, ctx):
+        # Initialize the reddit instance
+        reddit = asyncpraw.Reddit(client_id=REDDIT_CLIENT,
+                            client_secret=REDDIT_SECRET,
+                            user_agent='bot')
+
+        # Reddit API Calls
         subreddit = await reddit.subreddit("memes")
         all_subs = []
         top = subreddit.top(limit=35)
@@ -27,6 +28,7 @@ class reddit(commands.Cog):
         name = random_sub.title
         url = random_sub.url
 
+        # Embed Create 
         embed = discord.Embed(title=name, color=0xff00ea)
         embed.set_image(url=url)
         embed.add_field(
