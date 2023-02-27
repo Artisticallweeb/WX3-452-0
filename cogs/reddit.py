5c3ce73a2aab2ce4b6adbcce3223531b387a0790
Kay <davidanton037@gmail.com>
2023-02-27 12:24:01 -0500

    Update reddit.py

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -1,12 +1,12 @@
 import discord
-import praw
+import asyncpraw
 import random
 
 from discord.ext import commands
 from utils.constants import REDDIT_CLIENT, REDDIT_SECRET
 
 # Initialize the reddit instance
-reddit = praw.Reddit(client_id=REDDIT_CLIENT,
+reddit = asyncpraw.Reddit(client_id=REDDIT_CLIENT,
                      client_secret=REDDIT_SECRET,
                      ser_agent='bot')
 
@@ -17,13 +17,18 @@ class reddit(commands.Cog):
     # Random meme command 
     @commands.command(aliases=["memes"])
     async def meme(self, ctx):
-        memes_submissions = reddit.subreddit("dankmemes").new()
-        post_to_pick = random.randint(1, 100)
-        for i in range(0, post_to_pick):
-            submission = memes_submissions.__next__()
+        subreddit = await reddit.subreddit("memes")
+        all_subs = []
+        top = subreddit.top(limit=35)
+        async for submission in top:
+            all_subs.append(submission)
 
-        embed = discord.Embed(title='you found MEMES!!', color=0xff00ea)
-        embed.set_image(url=submission.url)
+        random_sub = random.choice(all_subs)
+        name = random_sub.title
+        url = random_sub.url
+
+        embed = discord.Embed(title=name, color=0xff00ea)
+        embed.set_image(url=url)
         embed.add_field(
             name="View Online",
             value=f"[Link]({submission.url})",
